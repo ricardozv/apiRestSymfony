362170c4bd94b09ca485ab7d5c8a4552cde76e4e
ricardozv <ricardozv@gmail.com>
2020-10-24 18:32:40 -0300

    conexao e adiçao com banco de dados

---
+++ geo/src/Controller/MedicosController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Medico;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MedicosController extends AbstractController
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/medicos", methods={"POST"})
+     */
+    public function novo(Request $request): Response
+    {
+        $corpoRequisicao = $request->getContent();
+        $dadoEmJson = json_decode($corpoRequisicao);
+
+        $medico = new Medico();
+        $medico->crm = $dadoEmJson->crm;
+        $medico->nome = $dadoEmJson->nome;
+
+        $this->entityManager->persist($medico);
+        $this->entityManager->flush();
+
+        return new JsonResponse($medico);
+    }
+
+    /**
+     * @Route("/medicos", methods={"GET"})
+     */
+    public function buscarTodos(): Response
+    {
+        $repositorioDeMedicos = $this
+            ->getDoctrine()
+            ->getRepository(Medico::class);
+
+        $medicoList = $repositorioDeMedicos->findAll();
+
+        return new JsonResponse($medicoList);
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={"GET"})
+     */
+    public function buscarUm(Request $request): Response
+    {
+        $id = $request->get('id');
+        $repositorioDeMedicos = $this
+            ->getDoctrine()
+            ->getRepository(Medico::class);
+        $medico = $repositorioDeMedicos->find($id);
+        $codigoRetorno = is_null($medico) ? Response::HTTP_NO_CONTENT : 200;
+
+        return new JsonResponse($medico, $codigoRetorno);
+    }
+}
